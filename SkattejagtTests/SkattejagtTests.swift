6783f3b5b16c8dcb1779c9b56261dfbfc5670680
Johanne Kristine Kappel Pauls <johannekristinekappelpauls@johannes-macbook-pro.local>
2018-05-02 08:05:22 +0200

    Initial Commit

---
+++ SkattejagtTests/SkattejagtTests.swift
@@ -0,0 +1,36 @@
+//
+//  SkattejagtTests.swift
+//  SkattejagtTests
+//
+//  Created by Johanne Kristine Kappel Pauls on 02/05/2018.
+//  Copyright © 2018 Johanne Kristine Kappel Pauls. All rights reserved.
+//
+
+import XCTest
+@testable import Skattejagt
+
+class SkattejagtTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
